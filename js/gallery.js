a896c542867f7823c65612abb58795dac921b0a5
kenegi <kenegi1000@gmail.com>
2025-04-03 19:06:47 +0900

    Add js

--- js/gallery.js
+++ js/gallery.js
@@ -4,15 +4,15 @@
 function activateGallery() {
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
     let mainImage  = document.querySelector("#gallery-photo img");
-    // 更新される画像情報
-    let galleryInfo = document.querySelector("#gallery-info");
-    let title       = galleryInfo.querySelector(".title");
-    let description = galleryInfo.querySelector(".description");
   
     thumbnails.forEach(function(thumbnail) {
+      // 大画像をプリロードする
+      let newImageSrc  = thumbnail.dataset.largeVersion;
+      let largeVersion = new Image();
+      largeVersion.src = mainImage.src;
+  
       thumbnail.addEventListener("click", function() {
         // クリックされた画像をメイン画像として設定する
-        let newImageSrc = thumbnail.dataset.largeVersion;
         mainImage.setAttribute("src", newImageSrc);
   
         // 現在の画像を変更する
@@ -20,6 +20,10 @@ function activateGallery() {
         thumbnail.parentNode.classList.add("current");
   
         // 画像の情報を更新する
+        let galleryInfo = document.querySelector("#gallery-info");
+        let title       = galleryInfo.querySelector(".title");
+        let description = galleryInfo.querySelector(".description");
+  
         title.innerHTML       = thumbnail.dataset.title;
         description.innerHTML = thumbnail.dataset.description;
       });
